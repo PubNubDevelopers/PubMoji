73d439f0c945bf9c7359142ea2dfb75ccc28eec1
Lovdeep Singh <lovdeep7@gmail.com>
2019-05-28 18:38:26 -0700

    Facebook style implementation of Emoji Bar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import AnimationScreen from './src/components/Animation/AnimationScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AnimationScreen);
