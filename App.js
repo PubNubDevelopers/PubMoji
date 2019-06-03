69601f8cd022deeac43e18673894cec4d394347a
Cameron Akhavan <32117738+Cakhavan@users.noreply.github.com>
2019-06-03 10:44:43 -0700

    Cam (#1)
    * updates
    
    * emoji
    
    * emoji animation
    
    * completed multiple emoji animations
    
    * modal
    
    * deleted packages
    
    * lovedeep sux
    
    * cleaning

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import hi from './boss.png';
+import PubNubReact from 'pubnub-react';
+import User from './Components/User.js';
+import * as Animatable from 'react-native-animatable';
+import Modal from "react-native-modal";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +14,322 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    //Pub Sub Keys
+    this.pubnub = new PubNubReact({
+        publishKey: 'pub-c-a64b528c-0749-416f-bf75-50abbfa905f9',
+        subscribeKey: 'sub-c-8a8e493c-f876-11e6-80ea-0619f8945a4f'
+    });
+
+    console.disableYellowBox = true;
+
+    //Base State
+    this.state = {
+      latitude: -6.270565,
+      longitude: 106.759550,
+      error:null,
+      users: new Map(),
+      emoji: 0
+    };
+
+    //Initialize PubNub Instance
+    this.pubnub.init(this);
+
+  }
+
+
+  //Subscribe to a PubNub Channel
+  componentWillMount() {
+    this.pubnub.subscribe({
+      channels: ['channel1'],
+      withPresence: true
+    });
+  }
+
+  //Unsubscribe PubNub Channel
+  componentWillUnmount() {
+    this.pubnub.unsubscribe({
+      channels: ['channel1']
+    });
+  }
+
+  //Track User GPS Data
+  componentDidMount() {
+
+    this.pubnub.getMessage('channel1', (msg) => {
+        coord = [msg.message.latitude,msg.message.longitude]; //Format GPS Coordinates for Payload
+        let oldUser = this.state.users.get(msg.message.uuid); //Obtain User's Previous State Object
+
+        //emojiCount
+        let emojiCount;
+        if(msg.message.emoji != 2){ //Add Payload Emoji Count to emojiCount
+          if(oldUser){
+            emojiCount = oldUser.emoji + msg.message.emoji;
+          }else{
+            emojiCount = msg.message.emoji;
+          }
+        }else{
+          emojiCount = 0; //reset EmojiCount to 0
+        }
+
+
+        let newUser = {uuid: msg.message.uuid, coords: coord, emoji: msg.message.emoji }; //User's Updated State
+        //Check If State Has Changed With User
+        if(!this.isEquivalent(oldUser, newUser)){
+          let tempMap = this.state.users;
+          newUser = {uuid: msg.message.uuid, coords: coord, emoji: emojiCount}; //add in the emoji count
+          //Add/Remove User depending on hideUser
+          if(msg.message.hideUser){
+            tempMap.delete(newUser.uuid)
+          }else{
+            tempMap.set(newUser.uuid, newUser);
+          }
+
+          this.setState({
+            users: tempMap
+          })
+        }
+    });
+
+
+    //Get Stationary Coordinate
+    navigator.geolocation.getCurrentPosition(
+      position => {
+       // console.log(position);
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        });
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 }
     );
+
+
+    //Track motional Coordinates
+    navigator.geolocation.watchPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        this.setState({ latitude,longitude });
+        this.pubnub.publish({
+          message: {latitude: this.state.latitude, longitude: this.state.longitude, uuid: this.pubnub.getUUID(), emoji: this.state.emoji},
+          channel: 'channel1'
+        });
+
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 1000
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  //Coordinate Setter
+  setRegion = () => ({
+   latitude: this.state.latitude,
+   longitude: this.state.longitude,
+   latitudeDelta: 0,
+   longitudeDelta: 0
+
+  });
+
+  //Decrement Emoji Count
+  hideEmoji = () => {
+        this.pubnub.publish({
+          message: {latitude: this.state.latitude, longitude: this.state.longitude, uuid: this.pubnub.getUUID(), emoji: -1},
+          channel: 'channel1'
+        });
+  };
+
+  //Increment Emoji Count
+  showEmoji = () => {
+        this.pubnub.publish({
+          message: {latitude: this.state.latitude, longitude: this.state.longitude, uuid: this.pubnub.getUUID(), emoji: 1},
+          channel: 'channel1'
+        });
+
+  };
+
+  //Reset Emoji Count
+  killEmoji = () => {
+       this.pubnub.publish({
+         message: {latitude: this.state.latitude, longitude: this.state.longitude, uuid: this.pubnub.getUUID(), emoji: 2},
+         channel: 'channel1'
+       });
+
+ };
+
+  showText = () => {
+    if(this.state.show == true){
+      this.setState({show: false});
+    }else{
+      this.setState({show:true});
+    }
+
+}
+
+  isEquivalent = (a, b) => {
+
+      if(!a || !b){
+        if(a === b) return true;
+        return false
+      }
+      // Create arrays of property names
+      var aProps = Object.getOwnPropertyNames(a);
+      var bProps = Object.getOwnPropertyNames(b);
+
+      // If number of properties is different,
+      // objects are not equivalent
+      if (aProps.length != bProps.length) {
+          return false;
+      }
+
+      for (var i = 0; i < aProps.length; i++) {
+          var propName = aProps[i];
+
+          // If values of same property are not equal,
+          // objects are not equivalent
+          if (a[propName] !== b[propName]) {
+              return false;
+          }
+      }
+
+      // If we made it this far, objects
+      // are considered equivalent
+      return true;
+    }
+
+
+  render() {
+
+       let usersArray = Array.from(this.state.users.values());
+
+    return (
+
+    <View style={styles.container}>
+
+      <View style={styles.view}>
+         <Modal isVisible={true}>
+           <View style={styles.modal}>
+             <Image style={styles.image} source={require('./Pubmoji.png')} style={{height: 180, width:250, }} />
+             <Text style={styles.text}>I am the modal content!</Text>
+           </View>
+         </Modal>
+       </View>
+       <MapView style={styles.map} region={this.setRegion()}>
+               { usersArray.map((item, index)=>(
+                 <Marker key={index} coordinate={{latitude: item.coords[0], longitude: item.coords[1]}}>
+
+                       {(item.emoji > 0 ) && <Animatable.View animation="fadeOutUp" duration={2000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-1 > 0 ) && <Animatable.View animation="fadeOutUp" duration={1500} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-2 > 0 ) && <Animatable.View animation="fadeOutUp" duration={1000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-3 > 0 ) && <Animatable.View animation="fadeOutUp" duration={2000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-4 > 0 ) && <Animatable.View animation="fadeOutUp" duration={1500} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-5 > 0 ) && <Animatable.View animation="fadeOutUp" duration={2000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-6 > 0 ) && <Animatable.View animation="fadeOutUp" duration={2000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.hideEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       {(item.emoji-7 > 0 ) && <Animatable.View animation="fadeOutUp" duration={2000} iterationCount={1} direction="normal" easing = "ease-out" onAnimationEnd={() => this.killEmoji()}>
+                          <Image source={require('./heart.png')} style={{height: 35, width:35, }} />
+                       </Animatable.View> }
+
+                       <Image source={require('./boss.png')} style={{height: 35, width:35, }} />
+                      <Button title="show Emoji" onPress={() => this.showEmoji()}/>
+                 </Marker>
+               )) }
+       </MapView>
+     </View>
+   );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  bubble: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent",
+
+  },
+  modal: {
+    flex: 1,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
+    padding: 100,
+    marginTop: 100,
+    marginBottom: 100,
+    marginLeft: 20,
+    marginRight: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+      color: '#3f2949',
+      marginTop: 10,
+      alignItems: 'center',
+   },
+   view: {
+      alignItems: 'center',
+      // backgroundColor: '#ede3f2',
+      padding: 100,
+   },
+   image: {
+      alignItems: 'center',
+      // backgroundColor: '#ede3f2',
+      padding: 100,
+   },
 });
