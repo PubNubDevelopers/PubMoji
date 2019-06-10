91d21e74758dfd133cd3da9df17fe7fbb33d62d9
Lovdeep Singh <lovdeep7@gmail.com>
2019-06-10 10:41:28 -0700

    Added message feature (#10)

--- android/app/src/main/java/com/pubmoji/MainApplication.java
+++ android/app/src/main/java/com/pubmoji/MainApplication.java
@@ -3,6 +3,7 @@ package com.pubmoji;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new FastImageViewPackage(),
             new RNSoundPackage(),
             new MapsPackage()
